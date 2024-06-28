2df84ff939122214c749fa0a417eaf8aee11ceba
Nguoi An Phu <nguoianphu@gmail.com>
2024-06-28 18:20:21 +0000

    init and readme

--- README_GITHUB.md
+++ README_GITHUB.md
@@ -1,1 +1,10 @@
 # Github code space
+
+https://github.com/nguoianphu/php-laravel-codespaces-devcontainer
+
+
+
+```
+
+composer install --ignore-platform-req=ext-intl --ignore-platform-req=ext-exif
+```
--- init_codespace.sh
+++ init_codespace.sh
@@ -3,7 +3,11 @@
 composer install
 
 php artisan key:generate
+
+php artisan storage:link
+
 php artisan migrate:fresh --seed
 
-npm install
-npm run dev
+npm install & npm run build
+
+php artisan serve # or npm run dev
